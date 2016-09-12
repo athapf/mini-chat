c930b5e0ffaacb4e82211ee2110b8b270f7e7472
athapf <a.thaler@thaler-pfedelbach.de>
2016-09-12 20:17:16 +0200

    add database module 'schema' with first table on H2 database

---
+++ mc-db/mc-db-schema/src/main/resources/db/local/develop/V1_1__insert_first_data.sql
@@ -0,0 +1,4 @@
+--
+--
+--
+INSERT INTO T_CHAT_MESSAGE (ID, TIMESTAMP, NICK, ROOM, MESSAGE) VALUES (SEQ_ID_CHAT_MESSAGE.nextval, '2016-09-03 14:39:17.023', 'ich', 'public', 'start a new conversation ...');
