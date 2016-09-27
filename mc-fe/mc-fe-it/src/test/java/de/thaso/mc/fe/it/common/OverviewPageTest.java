e4bebc40c2f626d7525413859b3097595c4955c4
athapf <a.thaler@thaler-pfedelbach.de>
2016-09-27 06:37:42 +0200

    new module system integration tests with selenium

---
+++ mc-fe/mc-fe-it/src/test/java/de/thaso/mc/fe/it/common/OverviewPageTest.java
@@ -0,0 +1,26 @@
+package de.thaso.mc.fe.it.common;
+
+import de.thaso.mc.fe.it.base.SeleniumTest;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * OverviewPageTest
+ *
+ * @author thaler
+ * @since 26.09.16
+ */
+public class OverviewPageTest extends SeleniumTest {
+
+    @Test
+    public void testOverviewPage() {
+        getDriver().get("http://localhost:9080/mc/minichat.xhtml");
+        final WebElement roomElement = getDriver().findElementById("myTable:0:room");
+        assertThat(roomElement.getText(),is("public"));
+        final WebElement nickElement = getDriver().findElementById("myTable:0:nick");
+        assertThat(nickElement.getText(),is("ich"));
+    }
+}
